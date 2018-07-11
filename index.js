b69b1c46894d131ac03f6a6440bc430c44e3e32f
syd2923 <sydloveskay@gmail.com>
2018-07-11 20:32:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 Variables defined in the test suite:
 - oldID
 - currentAge
-
+currentAge
 You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+var currentAge
+var oldID ;"1000000000";
+var newID ;"1000000000";
